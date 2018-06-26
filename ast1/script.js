d393ca41c346ea184c53a5607f06163076272673
Eddie023 <manish23chaulagain@gmail.com>
2018-06-26 18:36:25 +0545

    made ast1 loop infinite

--- ast1/script.js
+++ ast1/script.js
@@ -1,49 +1,3 @@
-//  function printPattern(n){
-//    var ast = "";
-//    for(i=0;i<n;i++)
-//    {
-//       ast += "*";
-//       console.log(ast);
-//    }
-//       // console.log(ast.length);
-//       counter = 1;
-//       num = 10;
-//       i = 1;
-//       while(  num!= i){
-//          ast = ast.slice(0,-1);
-//          console.log(ast);
-//         i+= 1;
-//
-//
-//       }
-//
-//
-//  }
-//
-// // printPattern(10);
-// var counter = 0;
-//
-//  var counterRef = setInterval(function(){
-//
-//    if(counter > 5) {
-//  clearInterval(counterRef)
-//  }
-//     printPattern(10);               //            if(increment == false){
-//                 
-//                ast= ast.slice(0,-1);
-//                if(ast.length === 1){
-//                    increment= true;
-//                    
-//                }
-                 
-////                              }
-                 
-//  counter++;
-//
-// },2000);
-//
-// alert("it's working");
-//  var item = 1;
 
 var animate = function(n) {
     var counter = 1;
@@ -51,9 +5,9 @@ var animate = function(n) {
     var increment = true;
     var counterref = setInterval(function() {
         
-        if (counter > 10) {
-          clearInterval(counterref); // console.log('countergreaterthan5');
-        }
+//        if (counter > 10) {
+//          clearInterval(counterref); // uncomment if you want a finite loop
+//        }
         
         if(increment == true){
             ast += "*";
